d67683171a0543c35c188494c2213ed66b3b12c7
abhishek56000 <abhishekgaur560@gmail.com>
2025-09-02 11:52:39 +0530

    Delete MicroServices directory

--- MicroServices/UserService/src/main/java/com/abhi/user/service/UserService/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abhi.user.service.UserService.repositories;
-
-import com.abhi.user.service.UserService.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
--- MicroServices/hotelService/src/main/java/com/abhi/hotel/hotelService/repositories/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abhi.hotel.hotelService.repositories;
-
-import com.abhi.hotel.hotelService.entities.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel,Integer> {
-}
